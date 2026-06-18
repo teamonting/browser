e89ae0c15a24fdd0c761420409632ec247a463b1
William Wong <compulim@hotmail.com>
2026-06-18 02:52:03 -0700

    Externalize @onting/stub

--- packages/browser/tsup.config.ts
+++ packages/browser/tsup.config.ts
@@ -15,7 +15,9 @@ export default defineConfig([
       browser: './src/browser/index.ts'
     },
     format: 'esm',
-    noExternal: ['@onting/rpc', '@onting/selenium-webdriver-message-port', '@onting/stub', 'message-port-rpc'],
+    // Intentionally left out @onting/stub. It should be inside import map and NOT bundled inside @onting/browser.
+    // Reason: when we release new version of @onting/stub, we don't need to bump @onting/browser.
+    noExternal: ['@onting/rpc', '@onting/selenium-webdriver-message-port', 'message-port-rpc'],
     outDir: 'static',
     sourcemap: true,
     target: ['chrome148']
