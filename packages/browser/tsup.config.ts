a5bd161ddcb5493f9b714489d7ffc52ce0986b52
William Wong <compulim@hotmail.com>
2026-06-18 01:33:46 -0700

    Move browser entrypoint to /static/ from /dist/

--- packages/browser/tsup.config.ts
+++ packages/browser/tsup.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: {
-      'bin-open': './src/bin-open/index.ts'
+      'bin-open': './src/bin-open/index.ts',
+      index: './src/index.ts'
     },
     format: 'esm',
     target: ['node24']
@@ -11,11 +12,11 @@ export default defineConfig([
   {
     dts: true,
     entry: {
-      browser: './src/browser/index.ts',
-      index: './src/index.ts'
+      browser: './src/browser/index.ts'
     },
     format: 'esm',
     noExternal: ['@onting/rpc', '@onting/selenium-webdriver-message-port', '@onting/stub', 'message-port-rpc'],
+    outDir: 'static',
     sourcemap: true,
     target: ['chrome148']
   }
