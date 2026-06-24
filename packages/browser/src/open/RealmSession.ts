a5bba35a98f6d66a261452d880fd26202e53bac6
William Wong <compulim@hotmail.com>
2026-06-24 01:24:31 -0700

    Better error handling

--- packages/browser/src/open/RealmSession.ts
+++ packages/browser/src/open/RealmSession.ts
@@ -40,113 +40,98 @@ class RealmSession<T extends Stub> extends EventTarget {
   }
 
   async #asyncConstructor(webDriver: WebDriver, realmInfo: RealmInfo, stubImplementation: StubImplementation<T>) {
-    let consoleEntryHandler: number | undefined;
     let logInspector: LogInspector | undefined;
     let messagePort: MessagePort | undefined;
     let scriptManager: ScriptManager | undefined;
     let teardown: () => void;
 
-    this.#abortController.signal.addEventListener(
-      'abort',
-      () => {
-        (async () => {
-          try {
-            await scriptManager?.close();
-          } catch (error) {
-            console.error(`[${shortenRealmId(realmInfo.realmId)}] Failed to close ScriptManager.`, error);
-          }
-        })();
-
-        (async () => {
-          try {
-            messagePort?.close();
-          } catch (error) {
-            console.error(`[${shortenRealmId(realmInfo.realmId)}] Failed to close MessagePort.`, error);
-          }
-        })();
-
-        consoleEntryHandler && logInspector?.removeCallback(consoleEntryHandler);
-
-        (async () => {
-          try {
-            await logInspector?.close();
-          } catch (error) {
-            console.error(`[${shortenRealmId(realmInfo.realmId)}] Failed to close LogInspector.`, error);
-          }
-        })();
-
-        try {
-          teardown?.();
-        } catch (error) {
-          console.error(`[${shortenRealmId(realmInfo.realmId)}] Failed to close MessagePort via BiDi.`, error);
-        }
-      },
-      { once: true }
-    );
-
     // TODO: It seems `selenium-webdriver@4.44.0` is bugged.
     //       If we use a shared `ScriptManager`, we will receive channel messages more than once.
     //       It seems if `onMessage()` is called twice, `selenium-webdriver` will call every `onMessage()` twice as well (4 times in total).
-    scriptManager = await getScriptManagerInstance(realmInfo.browsingContext, webDriver);
-
-    ({ messagePort } = await viaBiDi(scriptManager, { realmId: realmInfo.realmId }));
-
-    logInspector = await getLogInspectorInstance(webDriver, [realmInfo.browsingContext]);
-
-    consoleEntryHandler = await logInspector.onConsoleEntry(event => {
-      console.log(
-        `[${shortenRealmId(realmInfo.realmId)}]`,
-        ...event.args
-          .map(localValue => {
-            if (localValue && typeof localValue === 'object' && 'type' in localValue) {
-              // TODO: We should use `workthru` to deserialize.
-              if (localValue.type === 'string' && 'value' in localValue) {
-                return localValue.value;
-              }
-            }
-
-            return;
-          })
-          .filter(Boolean)
-      );
-    });
-
-    teardown = listen<StubImplementation<T>, T>(
-      stubImplementation,
-      {
-        browsingContext: await BrowsingContext(webDriver, { browsingContextId: realmInfo.browsingContext }),
-        webDriver
-      },
-      messagePort,
-      {
-        async marshal(value: unknown) {
-          return await workthru(value, async value =>
-            isWebElementLike(value)
-              ? [MARSHALLED_ELEMENT_SIGNATURE, { sharedId: await (value as unknown as WebElement).getId() }]
-              : value
-          );
-        },
-        async unmarshal(value: unknown) {
-          return await workthru(value, async value =>
-            isMarshalledElement(value) ? await unmarshalToWebElement(value, webDriver) : value
-          );
-        }
-      }
-    );
-
-    let elementTranslator: ElementTranslator | undefined;
-
     try {
-      elementTranslator = new ElementTranslator(webDriver, realmInfo);
+      scriptManager = await getScriptManagerInstance(realmInfo.browsingContext, webDriver);
+
+      this.#abortController.signal.addEventListener(
+        'abort',
+        () => scriptManager?.close().catch(error => console.error(error)),
+        { once: true }
+      );
+
+      ({ messagePort } = await viaBiDi(scriptManager, { realmId: realmInfo.realmId }));
+
+      this.#abortController.signal.addEventListener('abort', () => messagePort?.close(), { once: true });
+
+      logInspector = await getLogInspectorInstance(webDriver, [realmInfo.browsingContext]);
+
+      this.#abortController.signal.addEventListener(
+        'abort',
+        () => logInspector?.close().catch(error => console.error(error)),
+        { once: true }
+      );
+
+      const consoleEntryHandler = await logInspector.onConsoleEntry(event => {
+        console.log(
+          `[${shortenRealmId(realmInfo.realmId)}]`,
+          ...event.args
+            .map(localValue => {
+              if (localValue && typeof localValue === 'object' && 'type' in localValue) {
+                // TODO: We should use `workthru` to deserialize.
+                if (localValue.type === 'string' && 'value' in localValue) {
+                  return localValue.value;
+                }
+              }
+
+              return;
+            })
+            .filter(Boolean)
+        );
+      });
+
+      this.#abortController.signal.addEventListener('abort', () => logInspector?.removeCallback(consoleEntryHandler), {
+        once: true
+      });
+
+      teardown = listen<StubImplementation<T>, T>(
+        stubImplementation,
+        {
+          browsingContext: await BrowsingContext(webDriver, { browsingContextId: realmInfo.browsingContext }),
+          webDriver
+        },
+        messagePort,
+        {
+          async marshal(value: unknown) {
+            return await workthru(value, async value =>
+              isWebElementLike(value)
+                ? [MARSHALLED_ELEMENT_SIGNATURE, { sharedId: await (value as unknown as WebElement).getId() }]
+                : value
+            );
+          },
+          async unmarshal(value: unknown) {
+            return await workthru(value, async value =>
+              isMarshalledElement(value) ? await unmarshalToWebElement(value, webDriver) : value
+            );
+          }
+        }
+      );
+
+      this.#abortController.signal.addEventListener('abort', () => teardown(), { once: true });
+
+      let elementTranslator: ElementTranslator | undefined;
+
+      try {
+        elementTranslator = new ElementTranslator(webDriver, realmInfo);
+      } catch (error) {
+        // TODO: We blanket all errors about attaching the translator which may not be a good idea.
+        // In Firefox, the realm could be detached so fast we did not finish attach translator.
+        console.warn(`[${shortenRealmId(realmInfo.realmId)}] Realm detached immediately after attached`);
+
+        throw error;
+      }
+
+      this.#abortController.signal.addEventListener('abort', () => elementTranslator.close(), { once: true });
     } catch (error) {
-      // TODO: We blanket all errors about attaching the translator which may not be a good idea.
-      // In Firefox, the realm could be detached so fast we did not finish attach translator.
-      console.warn(`[${shortenRealmId(realmInfo.realmId)}] Realm detached immediately after attached`);
-
-      throw error;
+      console.error(error);
     }
-
-    this.#abortController.signal.addEventListener('abort', () => elementTranslator.close(), { once: true });
   }
 
   #abortController = new AbortController();
