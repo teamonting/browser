8ac7b818e6879d0585b4c3731953aa76f0421c0e
William Wong <compulim@hotmail.com>
2026-06-25 03:59:44 -0700

    Export eventing

--- packages/browser/src/open/index.ts
+++ packages/browser/src/open/index.ts
@@ -1,1 +1,2 @@
+export { RealmConsoleEvent, RealmErrorEvent, RealmEvent, WebDriverErrorEvent, WebDriverEvent } from './event.ts';
 export { default as WebDriverSession } from './WebDriverSession.ts';
