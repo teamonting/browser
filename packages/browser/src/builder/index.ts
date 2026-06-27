4549ad2c54ef83b3606fa04974ee4fd658320023
William Wong <compulim@hotmail.com>
2026-06-27 13:22:29 -0700

    Include DriverService

--- packages/browser/src/builder/index.ts
+++ packages/browser/src/builder/index.ts
@@ -1,2 +1,3 @@
 export { default as buildDriverService } from './buildDriverService.ts';
 export { default as buildWebDriver } from './buildWebDriver.ts';
+export type { DriverService } from './type.d.ts';
