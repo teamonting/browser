22f0bd9d724f338321c3e033cf5a21311db5679c
William Wong <compulim@hotmail.com>
2026-06-27 17:42:39 -0700

    Export RealmConsoleEventMethod

--- packages/browser/src/WebDriverSession/index.ts
+++ packages/browser/src/WebDriverSession/index.ts
@@ -1,2 +1,9 @@
-export { RealmConsoleEvent, RealmErrorEvent, RealmEvent, WebDriverErrorEvent, WebDriverEvent } from './event.ts';
+export {
+  RealmConsoleEvent,
+  type RealmConsoleEventMethod,
+  RealmErrorEvent,
+  RealmEvent,
+  WebDriverErrorEvent,
+  WebDriverEvent
+} from './event.ts';
 export { default as WebDriverSession } from './WebDriverSession.ts';
